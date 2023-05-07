954e14aa5fc9728e26c437249da58d37806de13b
Youssif Asaad <youssifasaadfci@gmail.com>
2023-05-07 20:58:54 +0300

    init

---
+++ Meeting Manegment System/Meeting Manegment System/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Meeting_Manegment_System.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
