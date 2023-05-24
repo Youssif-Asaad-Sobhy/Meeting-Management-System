3934e7453ed56b55de3148a3492f736024878179
Youssif Asaad <youssifasaadfci@gmail.com>
2023-05-24 20:31:05 +0300

    Test branch

---
+++ Meeting Manegment System/Meeting Manegment System/Controllers/CommitteeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Meeting_Manegment_System.Controllers
+{
+    public class CommitteeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
