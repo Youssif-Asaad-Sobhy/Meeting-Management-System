e4216fd47bf3c8ff456aac634138210faabe7cbf
Zeyad Abdelnaby <zeyadabdelnaby15@gmail.com>
2023-06-10 15:55:17 +0300

    all interfaces and repos

--- Meeting Manegment System/Meeting Manegment System/Interface/ICommitteeRepository.cs
+++ Meeting Manegment System/Meeting Manegment System/Interface/ICommitteeRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace Meeting_Manegment_System.Interface
+﻿using Meeting_Manegment_System.Models;
+
+namespace Meeting_Manegment_System.Interface
 {
     public interface ICommitteeRepository
     {
+        bool Add(Committee committee); 
+        bool Update(Committee committee);
+        bool Delete(Committee committee);
+        bool Save();
     }
 }
