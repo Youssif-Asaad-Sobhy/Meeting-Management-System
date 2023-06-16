219c44b826441f0a589412167448988036a4a7b2
Youssif Asaad <youssifasaadfci@gmail.com>
2023-06-17 02:08:30 +0300

    Added prefinal tricks

--- Meeting Manegment System/Meeting Manegment System/Interface/IMemberMeetingRepository.cs
+++ Meeting Manegment System/Meeting Manegment System/Interface/IMemberMeetingRepository.cs
@@ -5,6 +5,7 @@ namespace Meeting_Manegment_System.Interface
     public interface IMemberMeetingRepository
     {
         List<MemberMeeting> GetMemberMeetingsByMemberId(int memberId);
+        MemberMeeting GetMemberMeetingById(int Id);
         bool Add(MemberMeeting memberMeeting);
         bool Delete(MemberMeeting memberMeeting);
         bool Update(MemberMeeting memberMeeting);
