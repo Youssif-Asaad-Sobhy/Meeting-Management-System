f2e896ed17cacdc3c28ed31c10955aa4e5352dc3
Zeyad Abdelnaby <zeyadabdelnaby15@gmail.com>
2023-06-16 15:03:01 +0300

    userpage and forgetpassword page were added

---
+++ Meeting Manegment System/Meeting Manegment System/Controllers/UserPageController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Meeting_Manegment_System.Models;
+using Meeting_Manegment_System.Interface;
+using Meeting_Manegment_System.ViewModels;
+using Meeting_Manegment_System.Repository;
+
+namespace Meeting_Manegment_System.Controllers
+{
+    public class UserPageController : Controller
+    {
+        private  IMemberRepository _member;
+
+        public UserPageController(IMemberRepository memberRepository)
+        {
+            _member = memberRepository;
+        }
+        public IActionResult Index(int id)
+        {
+            Member member =_member.GetMemberById(id);
+            return View(member);
+        }
+        [HttpPost]
+        public IActionResult ForgetPassword(int id , string curpassword, string newpassword)
+        {
+            Member member = _member.GetMemberById(id); 
+            if (member.Password == curpassword)
+            {
+                member.Password = newpassword;
+                _member.Update(member);
+                ViewBag.Message = "Password changed successfully.";
+            }
+            else
+            {
+                ViewBag.Error = "Invalid current password.";
+            }
+            return View();
+        }
+    }
+}
